6b9e8271d059c5540d41e8c0b04a3979a5e73646
Wilker Lopes <wilkerlopes257@gmail.com>
2025-04-14 16:50:22 +0000

    Primeira etapa do jogo de trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -7,16 +8,95 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Definir locale PT-br
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    // Definição das variáveis para a primeira carta
+    char primeiraCartaEstado;
+    char primeiraCartaID[4];
+    char primeiraCartaCidade[50];
+    int primeiraCartaPopulacao;
+    float primeiraCartaArea;
+    float primeiraCartaPib;
+    int primeiraCartaPontosTuristicos;
+
+    // Definição das variáveis para a segunda carta
+    char segundaCartaEstado;
+    char segundaCartaID[4];
+    char segundaCartaCidade[50];
+    int segundaCartaPopulacao;
+    float segundaCartaArea;
+    float segundaCartaPib;
+    int segundaCartaPontosTuristicos;
+
+    // Cadastro da primeira carta
+    printf("===== Cadastro da primeira carta =====\n");
+    printf("Digite o estado (letra de A a H): ");
+    scanf(" %c", &primeiraCartaEstado);
+
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", primeiraCartaID);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", primeiraCartaCidade);
+
+    printf("Digite a população: ");
+    scanf("%d", &primeiraCartaPopulacao);
+
+    printf("Digite a área (em km²): ");
+    scanf("%f", &primeiraCartaArea);
+
+    printf("Digite o PIB (em bilhões de reais): ");
+    scanf("%f", &primeiraCartaPib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &primeiraCartaPontosTuristicos);
+
+
+    // Cadastro da segunda carta
+    printf("\n===== Cadastro da segunda carta =====\n");
+    printf("\nDigite o estado (letra de A a H): ");
+    scanf(" %c", &segundaCartaEstado);
+
+    printf("Digite o código da carta (ex: B02): ");
+    scanf("%s", segundaCartaID);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %s]", segundaCartaCidade);
+
+    printf("Digite a população: ");
+    scanf("%d", &segundaCartaPopulacao);
+
+    printf("Digite a área (em km²): ");
+    scanf("%f", &segundaCartaArea);
+
+    printf("Digite o PIB (em bilhões de reais): ");
+    scanf("%f", &segundaCartaPib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &segundaCartaPontosTuristicos);
+
+
+    // Exibição das informações da primeira carta
+    printf("\n\n===== Carta 1 =====\n");
+    printf("Código (ID): %s\n", primeiraCartaID);
+    printf("Estado: %c\n", primeiraCartaEstado);
+    printf("Nome da Cidade: %s\n", primeiraCartaCidade);
+    printf("População: %d\n", primeiraCartaPopulacao);
+    printf("Área: %.2f km²\n", primeiraCartaArea);
+    printf("PIB: R$ %.2f bilhões\n", primeiraCartaPib);
+    printf("Número de Pontos Turísticos: %d\n", primeiraCartaPontosTuristicos);
+
+
+    // Exibição das informações da segunda carta
+    printf("\n===== Carta 2 =====\n");
+    printf("Estado: %c\n", segundaCartaEstado);
+    printf("Código: %s\n", segundaCartaID);
+    printf("Nome da Cidade: %s\n", segundaCartaCidade);
+    printf("População: %d\n", segundaCartaPopulacao);
+    printf("Área: %.2f km²\n", segundaCartaArea);
+    printf("PIB: R$ %.2f bilhões\n", segundaCartaPib);
+    printf("Número de Pontos Turísticos: %d\n", segundaCartaPontosTuristicos);
 
     return 0;
 }
